12cd9808a41507a5fd4e64142c7bd6faefcb7022
taimaishuze <35374281+taimaishuze@users.noreply.github.com>
2018-03-27 15:02:55 -0500

    Update Chap9_10.sh

--- Chap9_10.sh
+++ Chap9_10.sh
@@ -18,10 +18,10 @@ until [ "$selection" = "0" ]; do
     echo "
     Program Menu
     1 - ifconfig/route -n/traceroute
-    2 - host/ping/ports
-    3 - netstat -nt/iptables/arp
-    4 - 
-
+    2 - host/ping/netstat -nt
+    3 - iptables/arp
+    4 - telnet/lsof
+    
     0 - exit program
 "
     echo -n "Enter selection: "
@@ -36,16 +36,12 @@ until [ "$selection" = "0" ]; do
 	echo "Pinging St Paul College....."; echo ""; ping 204.77.53.33; sleep 2; echo "";
 	echo "TCP ports and connections....."; echo ""; netstat -nt; sleep 2; echo "";
 	echo "Let's try other options."; echo ""; sleep 2; press_enter ;;
-      	3) echo "Displaying system info....."; echo ""; sudo dmesg; sleep 2; echo "";
-	echo "Displaying CPU information....."; echo ""; sudo dmesg | grep -i CPU; sleep 2; echo "";
-	echo "Displaying USB information....."; echo ""; sudo dmesg | grep -i USB; sleep 2; echo "";
-	echo "dmesg is best used with pipe less command.  Let's move on to another option"; sleep 2; press_enter ;;
-	4) echo "Update using sudo apt-get update"; echo ""; sleep 2;
-	echo "Turn on firewall using sudo ufw enable"; echo ""; sleep 2;
-	echo "Restart your OS with sudo shutdown -r now or set a countdown option"; echo ""; sleep 2;
-	echo "Shutdown your computer with sudo shutdown now or init 0"; echo ""; sleep 2;
-	echo "Try and restart or shut down your computer from the command line next time"; echo ""; sleep 2;
-        echo "For further help, use the 'man' command"; echo ""; sleep 2; press_enter ;;
+      	3) echo "Displaying iptables....."; echo ""; iptables -L; sleep 2; echo "";
+	echo "ARP tables....."; echo ""; arp; sleep 2; echo "";
+	echo "Let's move on to chapter 10....."; sleep 2; press_enter ;;
+	4) echo "Using telnet to connect to www.saintpaul.edu....."; telnet www.saintpaul.edu 80; sleep 2; echo "";
+	echo "List of programs using or listening to ports....."; lsof -i; sleep 2; echo "";
+	echo "This concludes the chapter 9 and 10 script....."; sleep 2; press_enter ;;
 	0) exit ;;
         *) echo "Please select 1, 2, 3, or 4 to view contents (0 to exit)"; press_enter
     esac
