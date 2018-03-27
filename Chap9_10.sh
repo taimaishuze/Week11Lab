726ebbe1e61e0ce6676b3e9f96bbf6c35b02ce66
taimaishuze <35374281+taimaishuze@users.noreply.github.com>
2018-03-27 15:22:21 -0500

    Update Chap9_10.sh

--- Chap9_10.sh
+++ Chap9_10.sh
@@ -30,7 +30,7 @@ until [ "$selection" = "0" ]; do
     case $selection in
         1) echo "Host IP address is....."; echo ""; ifconfig; sleep 2; echo "";
 	echo "Routing table....."; echo ""; route -n; sleep 2; echo "";
-	echo "traceroute to St Paul College....."; echo ""; traceroute www.saintpaul.edu; sleep 2; echo "";
+	echo "Installing traceroute and then traceroute to St Paul College....."; echo ""; sudo apt-get install traceroute && traceroute www.saintpaul.edu; sleep 2; echo "";
 	echo "Let's move on to the other options."; echo ""; sleep 2; press_enter ;;
         2) echo "Locating St Paul College IP address....."; echo ""; host www.saintpaul.edu; sleep 2; echo "";
 	echo "Pinging St Paul College....."; echo ""; ping 204.77.53.33; sleep 2; echo "";
@@ -40,7 +40,7 @@ until [ "$selection" = "0" ]; do
 	echo "ARP tables....."; echo ""; arp; sleep 2; echo "";
 	echo "Let's move on to chapter 10....."; sleep 2; press_enter ;;
 	4) echo "Using telnet to connect to www.saintpaul.edu....."; telnet www.saintpaul.edu 80; sleep 2; echo "";
-	echo "List of programs using or listening to ports....."; lsof -i; sleep 2; echo "";
+	echo "List of programs using or listening to ports....."; lsof -l | less; sleep 2; echo "";
 	echo "This concludes the chapter 9 and 10 script....."; sleep 2; press_enter ;;
 	0) exit ;;
         *) echo "Please select 1, 2, 3, or 4 to view contents (0 to exit)"; press_enter
